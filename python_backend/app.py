45f17b3303345f6b6300d8396fbc73f6b7b33e7a
Edu-p <eduardoaamorim0@gmail.com>
2024-08-19 14:33:38 -0300

    feat: route to return all grades of previous tests

--- python_backend/app.py
+++ python_backend/app.py
@@ -217,9 +217,20 @@ def max_english_level():
     else:
         return jsonify({"error": "No CEPR levels found for the user"}), 404
 
+@app.route('/return_all_tests', methods=['POST'])
+def return_all_tests():
+    """
+        POST in (6).0
+    """
+    data = request.get_json()
+    user_id = data['user_id']
+    grades = db.EnglishLevel.find({"user_id": user_id}).sort("_id", pymongo.ASCENDING)
 
+    grades_list = []
+    for grade in grades:
+        grades_list.append(grade['CEPR'])
 
-
+    return jsonify(grades_list), 200
 
 @app.route('/swagger.json')
 def swagger_json():
