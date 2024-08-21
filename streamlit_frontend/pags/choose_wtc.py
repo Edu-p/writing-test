ae88f96e417770d3ea8bc45bb9ea734f6dafeaed
Edu-p <eduardoaamorim0@gmail.com>
2024-08-21 07:57:34 -0300

    feat: page to choose the type of situation

---
+++ streamlit_frontend/pags/choose_wtc.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+def choose_what_type_of_chat():
+    st.title("")
+    st.subheader("")
+
+    st.markdown(
+        """
+        <style>
+        .stButton button {
+            width: 100%; 
+            height: 100px; 
+            font-size: 30px; 
+            margin: 10px;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+
+    
+    with st.container():
+        st.title("Choose what type of situation you want to be tested")
+        st.subheader("")
+        st.markdown('<div class="centered-buttons">', unsafe_allow_html=True)
+
+        if st.button("Report activity situation"):
+            st.session_state['page'] = 'report_activity_1'
+
+            st.rerun()
+        
+        st.markdown('</div>', unsafe_allow_html=True)
