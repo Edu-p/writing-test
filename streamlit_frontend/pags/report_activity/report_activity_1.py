7cd4ad8b5549ae4ea2798c6f1dac7decbf9e19f0
Edu-p <eduardoaamorim0@gmail.com>
2024-08-21 08:19:24 -0300

    feat: page to explain the test

---
+++ streamlit_frontend/pags/report_activity/report_activity_1.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import requests 
+import time
+
+def explanation_of_test():
+    # get explanation
+    response = requests.post(
+        # TODO: change when deploy
+        url='http://localhost:5000/explanations',
+        json={
+            'type': 'report'
+        }
+    ) 
+    if response.status_code == 200:
+        data = response.json()
+        # display the main button 
+        text_of_explanation = data['explanation']
+        st.text_area("", text_of_explanation)
+    else:
+        st.error("Problem in request.")
+    
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+        if st.button("Do the test"):
+            st.session_state['page'] = ''
+            st.rerun()
+    with col5:
+        if st.button("Previous page"):
+            st.session_state['page'] = 'choose_wtc'
+            st.rerun()
+
+
+
+
+
+
+
+
+
+
