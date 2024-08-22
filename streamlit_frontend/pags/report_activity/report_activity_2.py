b83b694e65f24937a0c704593e0d839c7c5b9b86
Edu-p <eduardoaamorim0@gmail.com>
2024-08-22 10:44:35 -0300

    fix: multiple requests issue when initializing page

--- streamlit_frontend/pags/report_activity/report_activity_2.py
+++ streamlit_frontend/pags/report_activity/report_activity_2.py
@@ -2,40 +2,43 @@ import streamlit as st
 import requests
 
 def report_test():
+    st.title("Chatbot Conversation")
+
+    # verifications in session state
+    if 'user_id' not in st.session_state:
+        st.session_state['user_id'] = '' 
     if 'conversation' not in st.session_state:
-        st.session_state['conversation'] = []
-
-    # if 'user_id' not in st.session_state:
-    #     st.session_state['user_id'] = ''
-    
-    # if 'entered_report_test' not in st.session_state:
-    #     st.session_state['entered_report_test'] = False
-
-    # if not st.session_state['entered_report_test']:
-    #     response_to_thread_id = requests.post(
-    #         url='http://localhost:5000/get_conversation',
-    #         json={
-    #             'user_id': st.session_state['user_id'],
-    #             'type_of_test': 'report'
-    #         }
-    #     )
+        st.session_state.conversation = []
+    if 'thread_id' not in st.session_state:
+        st.session_state['thread_id'] = None
         
-    #     data = response_to_thread_id.json()
-    #     thread_id = data['thread_id']
-    #     print(f"\n\n\n thread_id:{thread_id} \n\n")
 
-    #     st.session_state['entered_report_test'] = True
 
-    
-    # data = response_to_thread_id.json()
-    # thread_id = data['thread_id']
+    # get thread_id to start conversation
+    user_id = st.session_state['user_id']
+    thread_id = st.session_state['thread_id']
 
-    # print(f"\n\n\n thread_id:{thread_id} \n\n")
+    if thread_id:
+        print('Thread ID exists:', thread_id)
+    else:
+        response_to_thread_id = requests.post(
+            # TODO: change when deploy
+            url='http://localhost:5000/get_conversation',
+            json={
+                'user_id': user_id,
+                'type_of_test': 'report'
+            }   
+        )
+        
+        data = response_to_thread_id.json()
+        st.session_state['thread_id'] = data['thread_id']
+        thread_id = st.session_state['thread_id']
 
-    st.title('Conversation')
+        # print(f"\n\n\n thread_id:{thread_id} -- {type(thread_id)} \n\n")
 
-    conversation_display = "\n".join(st.session_state['conversation'])
-    st.text_area("", conversation_display, height=300, disabled=True)
+
+    conversation_display = "\n".join(st.session_state.conversation)
+    st.text_area("Conversation", conversation_display, height=300, disabled=True)
 
     user_input = st.text_input("Your message:")
 
@@ -43,12 +46,8 @@ def report_test():
         if user_input:
             st.session_state.conversation.append(f"You: {user_input}")
 
-            # requests = requests.post(
-            #     url=
-            # )
-
-            response = f"Bot: This is a response to '{user_input}'"
-            st.session_state['conversation'].append(response)
+            response = f"Bot: This is a response to '{user_input}', thread_id -> {thread_id}"
+            st.session_state.conversation.append(response)
 
             st.rerun()
 
