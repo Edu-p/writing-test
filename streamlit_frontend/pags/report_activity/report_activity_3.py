3d327803d7ec9b1b641f3f177715053cd00cb504
Edu-p <eduardoaamorim0@gmail.com>
2024-08-22 12:39:04 -0300

    feat: 3rd page of one single test

---
+++ streamlit_frontend/pags/report_activity/report_activity_3.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import requests 
+import time
+
+def show_conversation_summary():
+    st.title("")
+    st.subheader("")
+    # get explanation
+    response = requests.post(
+        # TODO: change when deploy
+        url='http://localhost:5000/get_english_level',
+        json={
+            "user_id": st.session_state['thread_id'],
+            "thread_id": st.session_state['user_id']
+        }
+    ) 
+    if response.status_code == 200:
+        data = response.json()
+        st.text_area("", f"You have achieved: {data['CEPR']}, you can be considered at {data['level']} level")
+    else:
+        st.error("Problem in request.")
