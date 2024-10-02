05c3d288e3ba59f86112d2afa4a7c7d3266399f3
Edu-p <eduardoaamorim0@gmail.com>
2024-10-02 07:31:47 -0300

    feat: adding correction of each user message
    - Just in chat of report activity

--- flask_backend/routes/chat.py
+++ flask_backend/routes/chat.py
@@ -2,10 +2,30 @@ from flask import request, jsonify, Blueprint
 from db import db
 from openai_client import client_openai
 from utils.helpers import get_completion_from_messages
-
+import re
+import json
 import pymongo
 
 chat_bp = Blueprint('chat', __name__)
+
+
+def generate_final_message(content: str, messages: list) -> str:
+    prompt = f"""
+        You are a tech lead. The purpose of this conversation is to collect a detailed report from your software engineer. Think about the better type of question that you can ask to gather comprehensive information about their activities, progress, challenges, and any support they might need.             Please, be short as possible on your responses. \
+        In addition i want to receive the next logic message of conversation and one text correcting ALL my errors. Like verb tenses,... \
+        Example of correction: "You wrote "I need to do a conversation with you." A more natural way to say this is "I need to have a conversation with you."" \
+        Format your response as a JSON object with "response" and "corr" \
+        I don't want you to send any other token outside of this json, just the json. \
+        Make your response as short as possible.
+
+        User's last message:
+        {content}
+    """
+    messages.append({"role": "user", "content": prompt})
+
+    response = get_completion_from_messages(messages)
+
+    return response
 
 
 @chat_bp.route('/chat', methods=['POST'])
@@ -22,7 +42,20 @@ def chat_response():
 
     messages.append({"role": "user", "content": content})
 
-    response = get_completion_from_messages(messages)
+    response = generate_final_message(content, messages)
+
+    print(f"reponse -> {response}")
+
+    match = re.search(
+        r'\{\s*"response"\s*:\s*".*"\s*,\s*"corr"\s*:\s*".*"\s*\}', response, re.DOTALL)
+
+    if match:
+        response_json = match.group(0)
+        print(f"Extracted JSON: {response_json}")
+    else:
+        print("No match found")
+
+    response_dict = json.loads(response_json)
 
     db.Conversations.insert_many([
         {
@@ -35,8 +68,8 @@ def chat_response():
             "thread_id": thread_id,
             "user_id": user_id,
             "role": "assistant",
-            "content": response
+            "content": response_dict['response']
         }
     ])
 
-    return jsonify({"response": response})
+    return jsonify({"response": response_dict['response'], "corr": response_dict['corr']})
--- streamlit_frontend/pags/report_activity/report_activity_2.py
+++ streamlit_frontend/pags/report_activity/report_activity_2.py
@@ -27,6 +27,8 @@ def report_test():
         st.session_state['send_button_clicked'] = False
     if 'user_input' not in st.session_state:
         st.session_state['user_input'] = ""
+    if "last_correction" not in st.session_state:
+        st.session_state['last_correction'] = "No correction yet."
 
     # get thread_id to start conversation
     user_id = st.session_state['user_id']
@@ -54,10 +56,15 @@ def report_test():
         st.text_area("Conversation", conversation_display,
                      height=300, disabled=True)
         st.session_state['step_of_conversation'] += 1
+
+        st.text_area("Correction of last message", 'No correction received yet.', height=100, disabled=True)
     else:
         conversation_display = "\n".join(st.session_state.conversation)
         st.text_area("Conversation", conversation_display,
                      height=300, disabled=True)
+        
+        last_correction = st.session_state.get('last_correction', 'No correction received yet.')
+        st.text_area("Correction of last message", last_correction, height=100, disabled=True)
 
     user_input = st.text_input(
         "Your message:", value=st.session_state['user_input'])
@@ -78,19 +85,25 @@ def report_test():
                     }
                 )
 
-                data = response_to_input.json()
-                llm_response = data['response']
+                if response_to_input.status_code == 200:
+                    data = response_to_input.json()
+                    llm_response = data['response']
+                    llm_correction = data['corr']
 
-                response = (f"Bot: {llm_response}\n")
-                print(f"Bot: {llm_response}', thread_id -> {thread_id}")
+                    response = (f"Bot: {llm_response}\n")
+                    print(f"Bot: {llm_response}', thread_id -> {thread_id}")
 
-                st.session_state['user_input'] = ""
+                    st.session_state['user_input'] = ""
 
-                st.session_state.conversation.append(response)
+                    st.session_state.conversation.append(response)
 
-                st.session_state['send_button_clicked'] = False
+                    st.session_state['last_correction'] = llm_correction
 
-                st.rerun()
+                    st.session_state['send_button_clicked'] = False
+
+                    st.rerun()
+                else:
+                    st.error('Error in response from backend')   
     else:
         time.sleep(2)
 
